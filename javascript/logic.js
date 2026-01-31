ffdeba9b9456bc979c40153ffe49e1217f4d509c
Luan <at.luan2915@gmail.com>
2026-01-31 14:34:15 -0300

    Atualização de estilo e adição de eventos para interação com teclado

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,4 +1,3 @@
-let characters = document.getElementsByClassName("char")
 let boatPositions = document.querySelectorAll(".boat-position");
 let boat = {
     direction: "left",
@@ -10,7 +9,7 @@ let boat = {
 document.addEventListener("click", (e) => {
 
     let target = e.target
-    if (target.className === "char"){
+    if (target.className === "cani" || target.className === "miss"){
         if (target.parentElement.className === "boat-position"){  
             removeFromBoat(target)
         }
@@ -18,11 +17,18 @@ document.addEventListener("click", (e) => {
             placeInBoat(target)
            
         }
-        
     }
 })
 
 function placeInBoat(image){
+    let parentId = image.parentElement.id
+    let n = parentId.replace(/[^0-9]/g, "")
+    console.log(n)
+
+    if (n > 6 && boat.direction === "left") return
+
+    if(n < 6 && boat.direction === "right") return
+
     for (let i = 0; i < boatPositions.length; i++ ){
         bp = boatPositions[i]
         let ch = bp.children
@@ -66,21 +72,129 @@ function sendBoat(){
             theresPassenger = true
         }
     }
-    if (boat.direction === "left"){
-        boat.direction = "right"
-        boat.boatDiv.style.left = "900px"
-        document.querySelector(".pointer-left").style.display =  "inline-block"
-        document.querySelector(".pointer-right").style.display =  "none"
+    if (theresPassenger){
+        if (boat.direction === "left"){
+            boat.direction = "right"
+            boat.boatDiv.style.left = "900px"
+            document.querySelector(".pointer-left").style.display =  "inline-block"
+            document.querySelector(".pointer-right").style.display =  "none"
+            gameRules()
 
+        }
+        else{
+            boat.direction = "left"
+            boat.boatDiv.style.left = "500px"
+            document.querySelector(".pointer-left").style.display =  "none"
+            document.querySelector(".pointer-right").style.display =   "inline-block"
+            gameRules()
+        }
     }
-    else{
-        boat.direction = "left"
-        boat.boatDiv.style.left = "500px"
-        document.querySelector(".pointer-left").style.display =  "none"
-        document.querySelector(".pointer-right").style.display =   "inline-block"
-    }
-    return theresPassenger;
+
+
 }
 
+function cleanBoat(){
+    console.log("start")
+    bps = document.querySelectorAll(".boat-position")
+    for (let i = 0; i < bps.length; i++){
+        innerImage = bps[i].children 
+        if (innerImage.length > 0){
+            removeFromBoat(innerImage[0]) 
+        }
+    } 
+}
 
+function gameRules(){
+    let gameOver = false
+    let missInLeft = 0;
+    let missInRight = 0;
+    let caniInLeft = 0 ;
+    let caniInRight = 0;
 
+    let inEarth = document.querySelectorAll(".position")
+    let inBoat = document.querySelectorAll(".boat-position")
+    for (let i = 0; i < inEarth.length; i++){
+       let children = inEarth[i].children
+        if (i<=5){
+            if (children.length > 0){
+                if (children[0].className === "miss"){
+                    missInLeft += 1
+                } else{
+                    caniInLeft += 1
+                }
+            }
+        }
+        else{
+             if (children.length > 0){
+                if (children[0].className === "miss"){
+                    missInRight += 1
+                } else{
+                    caniInRight += 1
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < inBoat.length; i++){
+        let children = inBoat[i].children
+        if (children.length > 0){
+            if (boat.direction == "left"){
+                if (children[0].className === "miss"){
+                    missInLeft += 1
+                } else{
+                    caniInLeft += 1
+                }
+            }
+            else{
+                if (children.length > 0){
+                if (children[0].className === "miss"){
+                    missInRight += 1
+                } else{
+                    caniInRight += 1
+                }
+            }
+        }
+    }
+}
+    console.log("Missionários na esquerda: " + missInLeft)
+    console.log("Canibais na esquerda: " + caniInLeft)
+    console.log("Missionários na direita: " + missInRight)
+    console.log("Canibais na direita: " + caniInRight)
+    console.log("----------------------------------------")
+    let CharatersInLeft = missInLeft + caniInLeft;
+    let CharatersInRight = missInRight + caniInRight;
+    if (missInLeft > 0 && missInRight > 0){
+        if(CharatersInLeft > 0 && CharatersInRight > 0){
+        if (missInLeft < caniInLeft || missInRight < caniInRight){
+            console.log("game-over")
+            gameOver = true
+        }
+    }
+    }
+   
+    if (gameOver){
+        document.querySelector(".controll").style.display = "none"
+        document.querySelector(".fundo-go").style.display = "block"
+    }
+}
+
+function closeWindow(){
+    document.querySelector(".fundo-go").style.display = "none"
+}
+
+function resetGame(){
+    location.reload()
+}
+
+addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight" || e.key=== "ArrowLeft"){
+        sendBoat()
+    }
+})
+
+addEventListener("keydown", (e)=> {
+    console.log(e)
+    if (e.key === " "){
+        cleanBoat()
+    }
+})
